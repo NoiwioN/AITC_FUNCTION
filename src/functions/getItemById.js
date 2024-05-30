15957aae97e291115863b5060def26a3854cb979
NoiwioN <mattias.zurbuchen@ict-campus.net>
2024-05-30 16:56:16 +0200

    Added getByItem

---
+++ src/functions/getItemById.js
@@ -0,0 +1,20 @@
+const { app, input } = require('@azure/functions');
+
+const cosmosInput = input.cosmosDB({
+    databaseName: 'DemoDatabase',
+    containerName: 'Items',
+    connection: 'CosmosDB',
+    sqlQuery: "select * from c where c.id={id}"
+});
+
+app.http('getItemById', {
+    methods: ['GET'],
+    authLevel: 'anonymous',
+    route:"items/{id}",
+    extraInputs:[cosmosInput],
+    handler: async (request, context) => {
+        const item = context.extraInputs.get(cosmosInput)
+
+        return { body: JSON.stringify(item),status:200 }
+    }
+});
