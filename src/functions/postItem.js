5aaa1338255ba924fd4dc26383cefe28b3cb6d50
NoiwioN <mattias.zurbuchen@ict-campus.net>
2024-05-30 15:44:10 +0200

    post

---
+++ src/functions/postItem.js
@@ -0,0 +1,25 @@
+const { app } = require('@azure/functions');
+
+const cosmosOutput = output.cosmosDB({
+    databaseName: 'DemoDatabase',
+    containerName: 'Items',
+    connection: 'CosmosDB',
+    createIfNotExists: true
+})
+
+app.http('postItem', {
+    methods: ['POST'],
+    authLevel: 'anonymous',
+    extraOutputs: [cosmosOutput],
+    route:"items",
+    handler: async (request, context) => {
+        const item = await request.json();
+        item.id=(Math.random()+1).toString(36)
+        context.extraOutputs.set(cosmosOutput,item)
+        return {
+            id:item.id,
+            title: item.title,
+            text: item.text
+         };
+    }
+});
