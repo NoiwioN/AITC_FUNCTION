68f3acd2accc87f7d8435661d23a360160d0b403
NoiwioN <mattias.zurbuchen@ict-campus.net>
2024-05-30 17:16:35 +0200

    Created delete, needs changes tho

---
+++ src/functions/deleteById.js
@@ -0,0 +1,24 @@
+const { app, input } = require('@azure/functions');
+const { CosmosClient } = require("@azure/cosmos");
+
+const cosmosInput = input.cosmosDB({
+    databaseName: 'DemoDatabase',
+    containerName: 'Items',
+    connection: 'CosmosDB',
+});
+
+
+app.http('deleteById', {
+    methods: ["DELETE"],
+    authLevel: 'anonymous',
+    route:"items/{id}",
+    extraInputs:  [cosmosInput],
+    handler: async (request, context) => {
+        const cosmosClient = new CosmosClient(/*{endpoint and key}*/ );
+        const container = cosmosClient.databaseName(cosmosInput.databaseName).container(/*containerId*/)
+        const itemId= request.url.split("/").pop()
+        const partitionKey = "placeholder"
+    const {resource} = await container.item(itemId/*,partitionKey*/).delete()
+        return { body: `Hello, ${name}!` };
+    }
+});
