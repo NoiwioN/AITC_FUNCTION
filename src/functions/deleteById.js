db180b076e7e46e1803fdd41e99c95b7067d7e55
NoiwioN <mattias.zurbuchen@ict-campus.net>
2024-06-05 15:43:26 +0200

    Delete fixed

--- src/functions/deleteById.js
+++ src/functions/deleteById.js
@@ -1,29 +1,27 @@
 const { app } = require("@azure/functions");
 const { CosmosClient } = require("@azure/cosmos");
- 
-const endpoint = "https://mz141829.documents.azure.com:443/";
-const key =
-  "NvFZ9aZt1ynqFGN5cGtgCtwy0fFFwhbnwRXY2RlOVcpKVOPbVtvOjJC3enrMlFx5X7h8qY62hmEbACDbZ7SqAw==";
+const account = process.env["CosmosDB"].split(";")
+const endpoint = account[0].split("=")[1]
+const key =account[1].split("=")[1]+"=="
  
 const client = new CosmosClient({ endpoint, key });
- 
+ console.log("Endpoint: " + endpoint)
+
 const databaseId = "DemoDatabase";
 const containerId = "Items";
+
  
 app.http("deleteItem", {
   methods: ["DELETE"],
   authLevel: "anonymous",
   route: "items/{id}",
   handler: async (request, context) => {
-    console.log("COnnecting to database")
     const database = client.database(databaseId);
-    console.log("Getting container")
     const container = database.container(containerId);
-    console.log("Container:" + JSON.stringify(container))
- 
+    
     const itemId = request.params.id;
  
-    const { resource: deletedItem } = await container.item(itemId).delete();
+    const { resource: deletedItem } = await container.item(itemId, itemId).delete();
     return {
       body: JSON.stringify(deletedItem),
       status: 200,
